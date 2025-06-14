93fba3f5d94ce5334c7cc5f57f0166873f293a6f
Adam Gąsowski <gander@users.noreply.github.com>
2025-06-15 01:06:30 +0200

    chore: add PSR-7 and PSR-17 support with Nyholm and Loophp bridge integration

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    loophp\PsrHttpMessageBridgeBundle\PsrHttpMessageBridgeBundle::class => ['all' => true],
 ];
